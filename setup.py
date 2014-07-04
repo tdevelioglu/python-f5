8ca61baa760648a317288b117c57697f5a0c000e
Juliano Martinez <juliano.martinez@booking.com>
2014-07-04 13:11:23 +0200

    bump to 0.1.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='f5',
-    version='0.1.5',
+    version='0.1.6',
     install_requires=['bigsuds'],
     description='Python Library to interact with F5',
     author='Taylan Develioglu',
