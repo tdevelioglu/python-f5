32fd64065e0fe16fdbecd62bab464d0bf0ac88c2
Taylan Develioglu <taylan.develioglu@booking.com>
2014-10-03 10:57:09 +0200

    New release 0.1.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='f5',
-    version='0.1.13',
+    version='0.1.14',
     install_requires=['bigsuds'],
     description='Python Library to interact with F5',
     author='Taylan Develioglu',
